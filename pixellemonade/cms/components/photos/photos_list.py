bdd2c634cac5ed30beeb0d416129bff3cf18497b
Matthijs Zondervan <mzondervan@hotmail.nl>
2023-02-27 23:36:04 +0100

    fix the search speed

--- pixellemonade/cms/components/photos/photos_list.py
+++ pixellemonade/cms/components/photos/photos_list.py
@@ -7,7 +7,9 @@ class PhotosListView(UnicornView):
     photos = None
     search_input: str = None
     album_id = None
-    tags = None
+
+    class Meta:
+        javascript_exclude = ("photos", )  # this to prevent sending a lot of data to the browser
 
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)  # calling super is required
@@ -15,14 +17,13 @@ class PhotosListView(UnicornView):
         self.search()
 
     def search(self):
-        photos = Photo.objects.all()
+        photos = Photo.objects.all().prefetch_related('tags')
         if self.album_id:
             print(self.album_id)
             photos = photos.filter(in_album=self.album_id)
 
         if self.search_input:
-            self.tags = PhotoTag.objects.all().filter(name__icontains=self.search_input)
-            photos = photos.filter(tags__in=self.tags)
+            photos = Photo.objects.all().filter(tags__name__contains=self.search_input).prefetch_related('tags')
         self.photos = photos
 
     def updated_search_input(self, value):

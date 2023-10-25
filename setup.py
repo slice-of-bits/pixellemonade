bedd9e169562e67f91eec5b1d6f322bb267e0dbd
Matthijs Zondervan <mzondervan@hotmail.nl>
2023-10-25 21:09:57 +0200

    why does this not work

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
+
 setup(
-    # ...,
-    packages=find_packages(where="pixellemonade"),
-    package_dir={"": "pixellemonade"},
-    include_package_data=True
+    package_data={'pixellemonade': ['templates/*.html']}
 )

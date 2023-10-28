12af94cedad763e8149d306c6c530e7b8adc8f24
Matthijs Zondervan <mzondervan@hotmail.nl>
2023-10-28 16:18:50 +0200

    add requirents to the setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,14 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'django-hashid-field==3.3.7',
+        'django-imagekit==5.0.0',
+        'Pillow==9.4.0',
+        'django-ninja==0.22.2',
+        'exif==1.6.0',
+        'IPTCInfo3==2.1.4',
+        'user-agents==2.2.0',
+        'django-treebeard==4.7',
+    ]
 )
